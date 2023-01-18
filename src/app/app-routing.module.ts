3e8fd1cd0b4acea665a4ab83aa753560fe81fffc
jkkoloma <kolomajeremi60@gmail.com>
2023-01-18 18:03:18 +0000

    Creations des pages de gestion des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from './guard/authentication.guard';
+import { QuizresolverService } from './_Services/quizresolver.service';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),canActivate:[AuthenticationGuard] // On Active AuthenticationGuard pour vérifier si l'utilisateur s'est authentifier
   },
+
+
+
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'profile/:username',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule), canActivate:[AuthenticationGuard]  // On Active AuthenticationGuard pour vérifier si l'utilisateur s'est authentifier
+  },
+  {
+    path: 'resetpassword',
+    loadChildren: () => import('./reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
+  },
+  {
+    path: 'quiz/:id',
+    loadChildren: () => import('./quiz-detail/quiz-detail.module').then( m => m.QuizDetailPageModule), resolve: {resolvedPost: QuizresolverService}, canActivate: [AuthenticationGuard]  // On Active AuthenticationGuard pour vérifier si l'utilisateur s'est authentifier, Resolver pour s'assurer que nous avons les informations de Quiz avant de les afficher
+  },
+ 
+ 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy, RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+// components
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// guards
+import { AuthenticationGuard } from './guard/authentication.guard';
+
+// services
+import { QuizresolverService } from './_Services/quizresolver.service';
+import { AccountService } from './_Services/account.service';
+import { LoadingService } from './_Services/loading.service';
+import { QuizService } from './_Services/quiz.service';
+import { AlertService } from './_Services/alert.service';
+
+// intercepteurs
+import { AuthInterceptor } from './Interceptor/auth.interceptor';
+import { CacheInterceptor } from './Interceptor/cache.interceptor';
+
+// external modules
+import { NgxLoadingModule } from 'ngx-loading';
+
+
 
 @NgModule({
   declarations: [
@@ -22,10 +42,22 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
     IonicModule.forRoot(),
-    AppRoutingModule // Pour la Gestion de routage
+    AppRoutingModule, // Nos routagee
+    NgxLoadingModule.forRoot({})
   ],
   
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    AccountService,
+    LoadingService,
+    QuizService,
+    AlertService,
+    QuizresolverService,
+    AuthenticationGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
