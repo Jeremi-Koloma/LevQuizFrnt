5e428888d42a423869b1acd0aaf9120a8652c548
jkkoloma <kolomajeremi60@gmail.com>
2023-01-27 12:54:56 +0000

    Ajout message notification affiche ok

--- src/app/_Services/alert.service.ts
+++ src/app/_Services/alert.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { Subject } from 'rxjs';
-import { AlertType } from '../_Enum/alert-type'; // Notre classe AlertType dans le model
 import { Alert } from '../_Models/alert'; // Notre classe Alert dans le model
 
 @Injectable({
@@ -11,13 +11,17 @@ export class AlertService {
   // Créeons une variable qu'on peut envoyé partout
   public alerts : Subject<Alert> = new Subject();
 
-  constructor() { }
+  constructor(private toastController : ToastController) { }
 
   // une fonction qu'on poura appélé pour envoyer une alerte dans l'application
-  showAlert(message: string, alterType: AlertType): void {
-    // créons une variable qui va prendre les deux variable
-    const alert = new Alert(message, alterType);
-    this.alerts.next(alert);
+  
+  async showAlert(msg:string,type:string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 3000,
+      color:type
+    });
+    toast.present();
   }
 
 
